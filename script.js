bd4173b5efc4e6611ce57791e0789773ee6f9f22
Cadu65 <carlinhos2jeeh@gmail.com>
2024-09-26 11:29:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function solicitaPerguntas() {
+    //Solicitar Pergunta 
+    let Quest1 = prompt("Em que ano o Brasil venceu a primeira copa do mundo?");
+
+    //Solicitar Pergunta
+    let Quest2 = prompt("O que tava escrito na faixa do neymar na comemoração no Estadio Olimpico de Berlim?")
+
+    //Solicitar Pergunta
+    let Quest3 = prompt("Qual jogador tem o maior números de gols da história?")
+
+    //Exibe as respostas
+    alert("Sua resposta da questão 1 foi: "+ Quest1 +". Porém resposta correta é: 29 de junho de 1958, na Suécia.\n " + "Sua resposta da questão 2 foi: "+ Quest2 +". Porém a resposta correta é: 100% Jesus.\n " + "Sua resposta da questão 3 foi: "+ Quest3 +". Porém a resposta correta é: Cristiano Ronaldo com 900 Gols.\n")
+
+
+}
